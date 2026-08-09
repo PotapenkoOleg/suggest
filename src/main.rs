2ca96baf7e9140628f6b840de08cbda58d4aa2b2
Oleg Potapenko <PotapenkoOleg@gmail.com>
2026-08-09 00:16:54 -0400

    Swagger/OpenAPI support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi(paths(index))]
+struct ApiDoc;
+
+#[utoipa::path(
+    get,
+    path = "/",
+    responses((status = 200, description = "OK"))
+)]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(|| async { HttpResponse::Ok().finish() }))
+        App::new()
+            .route("/", web::get().to(index))
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-docs/openapi.json", ApiDoc::openapi()),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
