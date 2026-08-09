d3d60192ebf8bbfdf393b8f9eb6a82bb33453d79
Oleg Potapenko <PotapenkoOleg@gmail.com>
2026-08-09 00:09:39 -0400

    Actix web starter

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(|| async { HttpResponse::Ok().finish() }))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
